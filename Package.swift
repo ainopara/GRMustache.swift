7fe70acf4f2a2e309dd56b4a2738ae1fcbdbbd12
张行 <zhanghang@globalegrow.com>
2019-01-24 16:58:20 +0800

    fix package.swift version error

--- Package.swift
+++ Package.swift
@@ -1,5 +1,28 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Mustache"
-) 
+    name: "GRMustache.swift-feature-swift4",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "GRMustache.swift-feature-swift4",
+            targets: ["GRMustache.swift-feature-swift4"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "GRMustache.swift-feature-swift4",
+            dependencies: []),
+        .testTarget(
+            name: "GRMustache.swift-feature-swift4Tests",
+            dependencies: ["GRMustache.swift-feature-swift4"]),
+    ]
+)
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import GRMustache_swift_feature_swift4Tests
+
+var tests = [XCTestCaseEntry]()
+tests += GRMustache_swift_feature_swift4Tests.allTests()
+XCTMain(tests)
